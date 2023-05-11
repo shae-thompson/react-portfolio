2f33a26621dad1df84ef4cccd37fb0fe97546698
mushroom <shaelah.mai@gmail.com>
2023-05-11 19:10:22 +1000

    adding to portfolio, contact and resume

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Me from '../../assets/me.jpeg'
 
 const styles = {
   page: {
@@ -19,15 +20,22 @@ const styles = {
     fontFamily: 'serif',
     color: 'black',
   },
+
+  image: {
+    borderRadius: '10px',
+  }
 }
 
 export default function Home() {
   return (
-    <div style={styles.page}>
+    <div class ='container' style={styles.page}>
       <h1 style={styles.heading}>About Me</h1>
-      <p style={styles.body}>
-      Hello! I am Shae, a 22-year-old student studying law at QUT and completing University of Adelaide coding bootcamp. I am hoping to one day work as in-house counsel within the tech sector. I wanted to learn more about tech and coding, whilst also creating an opportunity for me to work within the sector whilst completing my studies. So, I chose to dive head first into the bootcamp with no prior knowledge and just hoping for the best.  In my spare time I like to hang out with my dog, go for coffee walks with friends or tend to my many house plants! 🐶☕🌱
-      </p>
+      <div class='row'>
+        <img src={Me} class="col-2"></img>
+        <p style={styles.body} class='col'>
+        Hello! I am Shae, a 22-year-old student studying law at QUT and completing University of Adelaide coding bootcamp. I am hoping to one day work as in-house counsel within the tech sector. I wanted to learn more about tech and coding, whilst also creating an opportunity for me to work within the sector whilst completing my studies. So, I chose to dive head first into the bootcamp with no prior knowledge and just hoping for the best.  In my spare time I like to hang out with my dog, go for coffee walks with friends or tend to my many house plants! 🐶☕🌱
+        </p>
+        </div>
     </div>
   );
 }
